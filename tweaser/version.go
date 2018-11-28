c601341a6cd072889431405693119f9d36b7736a
E Camden Fisher <fish@fishnix.net>
2018-11-28 07:35:56 -0500

    gitignore fix, add tweaser package

---
+++ tweaser/version.go
@@ -0,0 +1,9 @@
+package tweaser
+
+// Version is the main version number that is being run at the moment.
+const Version = "No version set"
+
+// VersionPrerelease is a pre-release marker for the version. If this is ""
+// then it means that it is a final release. Otherwise, this is a pre-release
+// such as "dev" (in development), "beta", "rc1", etc.
+const VersionPrerelease = ""
