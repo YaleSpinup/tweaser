2c48e2226ac1879096e9c39a7057e75891346e09
E Camden Fisher <fish@fishnix.net>
2018-11-29 11:18:44 -0500

    cant set constants... duh

--- tweaser/version.go
+++ tweaser/version.go
@@ -1,15 +1,15 @@
 package tweaser
 
 // Version is the main version number that is being run at the moment.
-const Version = "No version set"
+var Version = "No version set"
 
 // VersionPrerelease is a pre-release marker for the version. If this is ""
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-const VersionPrerelease = ""
+var VersionPrerelease = ""
 
 // BuildStamp is the build timestamp
-const BuildStamp = "No BuildStamp Provided"
+var BuildStamp = "No BuildStamp Provided"
 
 // GitHash is the git hash ;)
-const GitHash = "No Git Commit Provided"
+var GitHash = "No Git Commit Provided"
